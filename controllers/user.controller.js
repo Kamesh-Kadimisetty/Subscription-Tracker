a43eda3081ae55763745bffcbd173ea8f030c23c
Kamesh-Kadimisetty <323103311022@gvpce.ac.in>
2026-01-20 23:35:46 +0530

    feat: Added Authorization contollers

---
+++ controllers/user.controller.js
@@ -0,0 +1,25 @@
+import User from '../models/user.model.js';
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.json({
+            success: true,
+            data: users
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id).select('-password');
+        res.json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        next(error);
+    }
+}
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config/env.js';
+
+export const authorize = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({
+                success: false,
+                message: "No token provided"
+            });
+        }
+
+        const token = authHeader.split(' ')[1];
+        
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "No token provided"
+            });
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized access",
+            error: error.message
+        });
+    }
+};
